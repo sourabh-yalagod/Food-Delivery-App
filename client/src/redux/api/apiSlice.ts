c061a0153ee603d026740e178da3d06b39ec7814
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-01-08 00:17:06 +0530

    RTK Query setup

---
+++ client/src/redux/api/apiSlice.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://api.example.com",
+        prepareHeaders: (headers) => {
+            const token = localStorage.getItem("token");
+            if (token) {
+                headers.set("authorization", `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    tagTypes: ["User", "Post"],
+    endpoints: () => ({}),
+});
